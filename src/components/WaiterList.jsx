6f4d4a6e61d5db7a448b2f474d14122837920342
unknown <dmwrqws@gmail.com>
2023-07-24 20:39:05 +0300

    change

--- src/components/WaiterList.jsx
+++ src/components/WaiterList.jsx
@@ -1,35 +1,34 @@
-import * as React from 'react';
-import ButtonRemove from './ButtonRemove';
+import * as React from "react";
+import ButtonRemove from "./ButtonRemove";
 
- function WaiterList(props) {
+function WaiterList(props) {
   return (
     <div className="list grid">
       <h2>רשימת עובדים</h2>
-  {props.list.map((e)=>{
-    return(
-     <div className="name flexRow bet" key={props.name}>
-        <h3>   {e.name}
-           </h3>
-          <div className="flexCol">
-          <p>   
-           {e.houer}:{e.dateStart}</p>
-           <p>   
-           {e.toHouer}:{e.dateEnd}</p>
+      {props.list.map((e) => {
+        return (
+          <div className="name flexRow bet" key={props.name}>
+            <h3> {e.name}</h3>
+
+            <p>{e.houer}</p>
+            <p>{e.toHouer}</p>
+
+            <p>
+              {e.job == "Washing"
+                ? "שוטף"
+                : e.job == "cook"
+                ? "טבח"
+                : e.job == "waiter"
+                ? "מלצר"
+                : e.job == "hostess"
+                ? "מארחת"
+                : ""}
+            </p>
+            <ButtonRemove func={props.removeWaiter} waiter={e.name} />
           </div>
-           <p>  
-            {e.job == 'Washing'? 'שוטף':
-            e.job == 'cook' ? 'טבח':
-            e.job == 'waiter' ? 'מלצר':
-            e.job == 'hostess'? 'מארחת':''
-             }
-           
-           </p>
-           <ButtonRemove func={props.removeWaiter} waiter={e.name} />
-     </div>
-    )
-  })}
+        );
+      })}
     </div>
-  
   );
 }
-export default WaiterList
+export default WaiterList;
